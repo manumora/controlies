06654ef6704fe1733d2ed96d0a355a13c4e33ec0
japastor <alfonso.pastor@gmail.com>
2014-11-12 10:14:12 +0100

    Adaptaciones wheezy

--- puppet/instala_controlies/manifests/init.pp
+++ puppet/instala_controlies/manifests/init.pp
@@ -15,7 +15,8 @@ class instala_controlies {
    }
    exec { "instala_controlies_client":
 		path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-		command => "dpkg -i --force-confnew  --force-overwrite $paquete_client; apt-get -f -y install ",
+                command => "apt-get remove --purge controlies-client; dpkg -i --force-confnew  --force-overwrite $paquete_client; apt-get -f -y install ",
+#		command => "dpkg -i --force-confnew  --force-overwrite $paquete_client; apt-get -f -y install ",
 		cwd => "/var/cache",
 		unless => "dpkg -l | grep controlies-client | grep $version | grep ^ii",
 		require => File["/var/cache/$paquete_client"],
@@ -25,6 +26,7 @@ class instala_controlies {
    #Paquete thinclients
    #################################
    case $use {
+        #Servidores squeeze
         "ltsp-server": {
 		file {"/opt/ltsp/i386/var/cache/$paquete_thinclient":
 			owner => root, group => root, mode => 755,
@@ -46,7 +48,29 @@ class instala_controlies {
 			refreshonly => true,
 		}
 	}
+        #Servidores wheezy
+        "ltsp-wheezy": {
+                file {"/opt/ltsp/i386/var/cache/$paquete_thinclient":
+                        owner => root, group => root, mode => 755,
+                        source => "puppet:///instala_controlies/$paquete_thinclient",
+                        notify => Exec["instala_controlies_thinclient"]
+                }
+
+                exec { "instala_controlies_thinclient":
+                        path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+                        command => "chroot /opt/ltsp/i386 /usr/bin/dpkg -i --force-confnew  --force-overwrite /var/cache/$paquete_thinclient",
+                                   #No se incluye el montaje de proc en el comando. Si se pone daba problemas en la instalacion del paquete.
+                        unless => "chroot /opt/ltsp/i386 dpkg -l | grep controlies-thinclient | grep $version | grep ^ii",
+                        require => File["/opt/ltsp/i386/var/cache/$paquete_thinclient"],
+                        notify => Exec["actualiza-imagen-controlies"]
+                }
+
+                exec { "actualiza-imagen-controlies":
+                        command => "/usr/sbin/ltsp-update-image",
+                        refreshonly => true,
+                }
+        }
+
 	default: {}
    }    
 }
-
