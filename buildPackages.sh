65fa6305c2923b72e090a88759bd52219fffa2fe
Manu <manu@pcmanu.santaeulalia>
2014-11-21 10:12:56 +0100

    modified:   buildPackages.sh 	deleted:    debian/controlies-ltspserver.debhelper.log 	modified:   packages/controlies-client_0.6.6-2_all.deb 	modified:   packages/controlies-ltspserver_0.6.6-2_all.deb 	modified:   packages/controlies-thinclient_0.6.6-2_all.deb 	modified:   packages/controlies_0.6.6-2_all.deb 	modified:   puppet/instala_controlies.zip 	modified:   puppet/instala_controlies/files/controlies-client_0.6.6-2_all.deb 	modified:   puppet/instala_controlies/files/controlies-ltspserver_0.6.6-2_all.deb 	modified:   puppet/instala_controlies/files/controlies-thinclient_0.6.6-2_all.deb 	modified:   puppet/instala_controlies/files/controlies_0.6.6-2_all.deb

--- buildPackages.sh
+++ buildPackages.sh
@@ -28,13 +28,16 @@ clear_cache(){
 	rm -rf debian/controlies
 	rm -rf debian/controlies-thinclient
 	rm -rf debian/controlies-client
+	rm -rf debian/controlies-ltspserver
 	rm -rf debian/controlies.debhelper.log
 	rm -rf debian/controlies-thinclient.debhelper.log
 	rm -rf debian/controlies-client.debhelper.log
+	rm -rf debian/controlies-ltspserver.debhelper.log
 	rm -rf debian/*.debhelper
 	rm -rf debian/controlies.substvars
 	rm -rf debian/controlies-thinclient.substvars
 	rm -rf debian/controlies-client.substvars
+	rm -rf debian/controlies-ltspserver.substvars
 	rm -rf debian/files
 	find ./ -iname "*.pyc" -print0 | xargs -0 rm -rf
 	find ./ -iname "*.*~" -print0 | xargs -0 rm -rf
