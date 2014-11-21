58b86e93daa26ed89b8878aa6b6b972cd536dee6
Manu <manu@pcmanu.santaeulalia>
2014-11-21 10:10:23 +0100

    modified:   debian/controlies-client.install 	modified:   debian/controlies-ltspserver.install 	new file:   packages/controlies-client_0.6.6-2_all.deb 	new file:   packages/controlies-ltspserver_0.6.6-2_all.deb 	new file:   packages/controlies-thinclient_0.6.6-2_all.deb 	new file:   packages/controlies_0.6.6-2_all.deb 	modified:   puppet/instala_controlies.zip 	new file:   puppet/instala_controlies/files/controlies-client_0.6.6-2_all.deb 	new file:   puppet/instala_controlies/files/controlies-ltspserver_0.6.6-2_all.deb 	new file:   puppet/instala_controlies/files/controlies-thinclient_0.6.6-2_all.deb 	new file:   puppet/instala_controlies/files/controlies_0.6.6-2_all.deb 	modified:   puppet/instala_controlies/manifests/init.pp

--- puppet/instala_controlies/manifests/init.pp
+++ puppet/instala_controlies/manifests/init.pp
@@ -1,7 +1,7 @@
 
 class instala_controlies {
 
-   $version="*"
+   $version="0.6.6-2"
    $paquete_client="controlies-client_${version}_all.deb"
    $paquete_thinclient="controlies-thinclient_${version}_all.deb"
 
