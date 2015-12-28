b1dedc473037a780f2ffede2911a0b294c58b37a
Manu <manu@jessie>
2015-12-28 19:23:34 +0100

    modified:   applications/controlies/controllers/default.py 	modified:   applications/controlies/models/db.py 	modified:   packages/controlies-client_0.7.0-0_all.deb 	modified:   packages/controlies-ltspserver_0.7.0-0_all.deb 	modified:   packages/controlies-thinclient_0.7.0-0_all.deb 	modified:   packages/controlies_0.7.0-0_all.deb 	modified:   puppet/instala_controlies.zip 	modified:   puppet/instala_controlies/files/controlies-client_0.7.0-0_all.deb 	modified:   puppet/instala_controlies/files/controlies-ltspserver_0.7.0-0_all.deb 	modified:   puppet/instala_controlies/files/controlies-thinclient_0.7.0-0_all.deb 	modified:   puppet/instala_controlies/files/controlies_0.7.0-0_all.deb

--- applications/controlies/models/db.py
+++ applications/controlies/models/db.py
@@ -88,14 +88,6 @@ crud.settings.auth = None                      # =auth to enforce authorization 
 ## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
 ## >>> for row in rows: print row.id, row.myfield
 #########################################################################
-import applications.controlies.modules.LdapConnection as LdapConnection
-
-if session.secureAuth=="on":
-    #Secure LDAP Auth
-    auth.settings.login_methods=[ldap_auth( server='localhost', base_dn='ou=People,dc=instituto,dc=extremadura,dc=es',secure=LdapConnection.ldap_secure,cert_path=LdapConnection.ldap_cert,mode='cn')]
-else:
-    #LDAP Auth
-    auth.settings.login_methods=[ldap_auth( server='localhost', base_dn='ou=People,dc=instituto,dc=extremadura,dc=es',secure=False,mode='cn')]
 
 
 #########################################################
