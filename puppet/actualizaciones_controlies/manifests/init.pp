64c0e77a21d24fc700fa3a8f0535a4871b659400
japastor <alfonso.pastor@gmail.com>
2016-02-25 21:23:43 +0100

    Adaptacion clientes a ubuntu

--- puppet/actualizaciones_controlies/manifests/init.pp
+++ puppet/actualizaciones_controlies/manifests/init.pp
@@ -6,46 +6,59 @@ class { actualizaciones_controlies: stage => final }
 
 class actualizaciones_controlies {
 
-  case $use {
-      "ltsp-server", "ltsp-wheezy": {
+  if $is_ltsp  == "SI" {
+        $tipo_imagen="ltsp"
+  } else {
+        $tipo_imagen="otro"
+  }
+ 
+  case $tipo_imagen {
+      "ltsp": {
 				# configuracion del despertado automatico de los thinclients para el seguimiento de los mismos.
-				        file {"/usr/share/controlies-client/":
+				        file {"/usr/share/controlies-ltspserver/":
 						owner => root, group => root, mode => 755,
 					ensure => directory,
-					before => File["/usr/share/controlies-client/despierta_thinclients"],
+					before => File["/usr/share/controlies-ltspserver/despierta_thinclients"],
 				}
 
-				file { "/usr/share/controlies-client/despierta_thinclients":
+				file { "/usr/share/controlies-ltspserver/despierta_thinclients":
 					owner => root, group => root, mode => 755,
-					source => "puppet:///actualizaciones_controlies/despierta_thinclients",
+					source => "puppet:///modules/actualizaciones_controlies/despierta_thinclients",
 				}
+
+                                file { "/usr/share/controlies-client/despierta_thinclients":
+                                        ensure=>absent
+                                }
+
 
 				cron { revision-matinal:
-					command => "/usr/share/controlies-client/despierta_thinclients",
+					command => "/usr/share/controlies-ltspserver/despierta_thinclients",
 					user    => root,
 					hour    => 8,
 					minute  => 15
 				}
 
-		#		cron { revision-recreo:
-		#		   command => "/usr/share/controlies-client/despierta_thinclients",
-		#		   user    => root,
-		#		   hour    => 11,
-		#		   minute  => 25
-		#		}
+#				cron { revision-recreo:
+#				   command => "/usr/share/controlies-ltspserver/despierta_thinclients",
+#				   user    => root,
+#				   hour    => 11,
+#				   minute  => 25
+#				}
 
-		#		cron { revision-vespertina:
-		#		   command => "/usr/share/controlies-client/despierta_thinclients",
-		#		   user    => root,
-		#		   hour    => 14,
-		#		   minute  => 45
-		#		}
+#				cron { revision-vespertina:
+#				   command => "/usr/share/controlies-ltspserver/despierta_thinclients",
+#				   user    => root,
+#				   hour    => 14,
+#				   minute  => 45
+#				}
 
 	}
+        "otro": {}
 	default: {}
     }
 
     package { "pkpgcounter":
+             name => pkpgcounter,
              ensure => installed,
              before => File["/usr/share/pyshared/pkpgpdls/zjstream.py"],
 
@@ -61,7 +74,7 @@ class actualizaciones_controlies {
     #Seguimiento impresión, parchea un bug en pkpgcounter.
     file {"/usr/share/pyshared/pkpgpdls/zjstream.py":
     	owner => root, group => root, mode => 644,
-    	source => "puppet:///actualizaciones_controlies/zjstream.py",
+    	source => "puppet:///modules/actualizaciones_controlies/zjstream.py",
     }        
 }
 
--- puppet/instala_controlies/manifests/init.pp
+++ puppet/instala_controlies/manifests/init.pp
@@ -1,19 +1,37 @@
 class instala_controlies {
 
-   $version="0.7.0-0"
+   $version="0.7.0-1"
    $paquete_client="controlies-client_${version}_all.deb"
    $paquete_ltspserver="controlies-ltspserver_${version}_all.deb"
    $paquete_thinclient="controlies-thinclient_${version}_all.deb"
 
+   file {"/usr/lib/ruby/vendor_ruby/facter/is_ltsp.rb":
+                                owner => root, group => root, mode => 644,
+                                source => "puppet:///modules/instala_controlies/is_ltsp.rb",
+   }
+
+   #Usamos el facter is_ltsp para determinar si el equipo es un LTSP
+   case $is_ltsp {
+      "SI" : {
+        $tipo_imagen="ltsp"
+      }
+      "NO" : {
+        $tipo_imagen="otro"
+      }
+      default:  { #Todavia no está el facter instalado, no podemos determinar el tipo de imagen
+        $tipo_imagen=""
+      }
+   }
+
    #################################
    #Paquete thinclients
    #################################
-   case $use {
+   case $tipo_imagen {
         #Servidores ltsp
-        "ltsp-server", "ltsp-wheezy", "aulatic-profesor-wheezy" : {
+        "ltsp" : {
                 file {"/opt/ltsp/i386/var/cache/$paquete_thinclient":
                         owner => root, group => root, mode => 755,
-                        source => "puppet:///instala_controlies/$paquete_thinclient",
+                        source => "puppet:///modules/instala_controlies/$paquete_thinclient",
                         notify => Exec["instala_controlies_thinclient"]
                 }
 
@@ -28,10 +46,10 @@ class instala_controlies {
 
 
 
-                #El comando para actualizar la imagen de los thinclient difiere entre squeeze y wheezy
+                #El comando para actualizar la imagen de los thinclient difiere entre squeeze/wheezy-ubuntu
         
                 case $use {
-                     "ltsp-wheezy", "aulatic-profesor-wheezy": {
+                     "ltsp-wheezy", "aulatic-profesor-wheezy", "ubuntu": {
                          $comandoupdateimagen="/usr/sbin/ltsp-update-image"
                      }
 
@@ -56,26 +74,25 @@ class instala_controlies {
 
 		file {"/var/cache/$paquete_ltspserver":
 			owner => root, group => root, mode => 755,
-			source => "puppet:///instala_controlies/$paquete_ltspserver",
+			source => "puppet:///modules/instala_controlies/$paquete_ltspserver",
 			notify => Exec["instala_controlies_ltspserver"]
 		}
 		exec { "instala_controlies_ltspserver":
 			path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 			command => "apt-get remove --purge controlies-ltspserver; dpkg -i --force-confnew  --force-overwrite $paquete_ltspserver; apt-get -f -y install ",
-		#		command => "dpkg -i --force-confnew  --force-overwrite $paquete_client; apt-get -f -y install ",
 			cwd => "/var/cache",
 			unless => "dpkg -l | grep controlies-ltspserver | grep $version | grep ^ii",
 			require => File["/var/cache/$paquete_ltspserver"],
 		}
 	}
-	default: {
+	"otro": {
 
 		   #################################
 		   #Paquete clients
 		   #################################
 		   file {"/var/cache/$paquete_client":
 				owner => root, group => root, mode => 755,
-				source => "puppet:///instala_controlies/$paquete_client",
+				source => "puppet:///modules/instala_controlies/$paquete_client",
 				notify => Exec["instala_controlies_client"]
 		   }
 		   exec { "instala_controlies_client":
