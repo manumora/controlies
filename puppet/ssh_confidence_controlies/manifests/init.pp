7bf6c6bb15d6069d59d6b76645e9ed8d4d6d4114
japastor <alfonso.pastor@gmail.com>
2018-09-07 17:02:22 +0200

    Adaptacion a Ubuntu Bionic. 0.7.0-8

--- puppet/ssh_confidence_controlies/manifests/init.pp
+++ puppet/ssh_confidence_controlies/manifests/init.pp
@@ -1,33 +1,22 @@
 class ssh_confidence_controlies {
 
-  file { "/root/.ssh/authorized_keys.controlies":
+   file { "/root/.ssh/authorized_keys.controlies":
                 source => "puppet:///modules/ssh_confidence_controlies/id_rsa.pub",
                 owner => root,
                 mode => 600
         }
 
-  exec { "add_controlies_key":
+   exec { "add_controlies_key":
                 path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
                 command => "cat /root/.ssh/authorized_keys.controlies >> /root/.ssh/authorized_keys; chattr +i /root/.ssh/authorized_keys",
                 unless => "grep -f /root/.ssh/authorized_keys.controlies /root/.ssh/authorized_keys",
                 require => File["/root/.ssh/authorized_keys.controlies"]
         }
 
-   #file {"/usr/lib/ruby/vendor_ruby/facter/file_exists.rb":
-   #                             owner => root, group => root, mode => 644,
-   #                             source => "puppet:///modules/instala_controlies/file_exits.rb",
-   #}
 
-   if file_exists ("/opt/ltsp/images/i386.img") == 1 {
-        $tipo_imagen="ltsp"
-   } else {
-        $tipo_imagen="otro"
-   }
-
-
-   case $tipo_imagen {
-        #Servidores ltsp
-        "ltsp" : {
+   case $ies_isltsp {  # Una imagen tipo ltsp puede ser un servidor de telefonica, una siatic, etc. Cualquiera que este
+                       # en un aula con thinclients. Debe ser declarado con un facter propio ies_isltsp en /etc/escuela2.0
+        "si","yes","true" : {
                 file { "/usr/share/controlies-ltspserver/.ssh":
                         ensure => "directory"
                 }
